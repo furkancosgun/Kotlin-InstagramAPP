8b6777813026c836d9a4a6bb9bc57ae9425a87a9
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-07 12:33:05 +0300

    Create Story.kt

---
+++ instagram/app/src/main/java/com/furkancosgun/instagram/Models/Story.kt
@@ -0,0 +1,38 @@
+package com.furkancosgun.instagram.Models
+
+data class Story(val name: String, val profileImg: String)
+class Stories {
+    companion object {
+        val stories = mutableListOf<Story>(
+            Story("Elon_musk", "https://i.insider.com/556e35f56bb3f7192014f8a2"),
+            Story(
+                "Linus_Torvalds",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SabkIoyFqotBIO9-g4sBGAHaE8%26pid%3DApi&f=1&ipt=6faf1c156b92ac41748ff3adfed9684c4f29e74506277017236371a7c0403268&ipo=images"
+            ), Story(
+                "Mark_Zuckerberg",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-seMXe-koxP8Dinf5egsQQHaE8%26pid%3DApi&f=1&ipt=d49a51eb6a5fca7874cf55992ec66558d4a6c1de0bd9804b11401fd7d0cf79eb&ipo=images"
+            ), Story("Elon_musk", "https://i.insider.com/556e35f56bb3f7192014f8a2"),
+            Story(
+                "Linus_Torvalds",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SabkIoyFqotBIO9-g4sBGAHaE8%26pid%3DApi&f=1&ipt=6faf1c156b92ac41748ff3adfed9684c4f29e74506277017236371a7c0403268&ipo=images"
+            ), Story(
+                "Mark_Zuckerberg",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-seMXe-koxP8Dinf5egsQQHaE8%26pid%3DApi&f=1&ipt=d49a51eb6a5fca7874cf55992ec66558d4a6c1de0bd9804b11401fd7d0cf79eb&ipo=images"
+            ), Story("Elon_musk", "https://i.insider.com/556e35f56bb3f7192014f8a2"),
+            Story(
+                "Linus_Torvalds",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SabkIoyFqotBIO9-g4sBGAHaE8%26pid%3DApi&f=1&ipt=6faf1c156b92ac41748ff3adfed9684c4f29e74506277017236371a7c0403268&ipo=images"
+            ), Story(
+                "Mark_Zuckerberg",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-seMXe-koxP8Dinf5egsQQHaE8%26pid%3DApi&f=1&ipt=d49a51eb6a5fca7874cf55992ec66558d4a6c1de0bd9804b11401fd7d0cf79eb&ipo=images"
+            ), Story("Elon_musk", "https://i.insider.com/556e35f56bb3f7192014f8a2"),
+            Story(
+                "Linus_Torvalds",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.SabkIoyFqotBIO9-g4sBGAHaE8%26pid%3DApi&f=1&ipt=6faf1c156b92ac41748ff3adfed9684c4f29e74506277017236371a7c0403268&ipo=images"
+            ), Story(
+                "Mark_Zuckerberg",
+                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-seMXe-koxP8Dinf5egsQQHaE8%26pid%3DApi&f=1&ipt=d49a51eb6a5fca7874cf55992ec66558d4a6c1de0bd9804b11401fd7d0cf79eb&ipo=images"
+            )
+        )
+    }
+}
